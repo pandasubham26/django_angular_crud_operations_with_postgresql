ffabda1a3459e3166c853ee71742ef70e5c43c1b
pandasubham26 <129537473+pandasubham26@users.noreply.github.com>
2023-12-14 17:37:05 +0530

    Delete backend directory

--- backend/users/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-# User -> id, name, email, gender
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'name', 'email', 'gender')
-        
